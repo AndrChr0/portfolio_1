6d83be20fa2982ed7465f471a75d3a509f97c2c9
Andreas Christiansen <andycchhrr@gmail.com>
2024-04-29 09:08:18 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import react from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { useState } from 'react'
+
 function App() {
 
+  const [click, setClick] = useState(false)
+
+  function renderText(){
+    setClick(!click)
+  }
+
   return (
-    <>
+    <ChakraProvider>
 <h1>Andreas sin kule portfolio</h1>
-    </>
+<Button colorScheme='red' onClick={()=>{
+  renderText()
+}}>Button</Button>
+{click ? <p>Andreas er kul</p> : null}
+    </ChakraProvider>
   )
 }
 
