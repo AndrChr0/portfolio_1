d5a26669e83b4adef0e823b6cfdf7effbbabf230
Andreas Christiansen <andycchhrr@gmail.com>
2024-04-30 09:00:28 +0200

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import { ChakraProvider } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
-import { useState } from 'react'
+import { ChakraProvider } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { useState } from "react";
 
 function App() {
+  const [click, setClick] = useState(false);
 
-  const [click, setClick] = useState(false)
-
-  function renderText(){
-    setClick(!click)
+  function renderText() {
+    setClick(!click);
   }
 
   return (
     <ChakraProvider>
-<h1>Andreas sin kule portfolio</h1>
-<Button colorScheme='red' onClick={()=>{
-  renderText()
-}}>Button</Button>
-{click ? <p>Andreas er kul</p> : null}
+      <h1>Andreas sin kule portfolio</h1>
+      <Button
+        colorScheme="red"
+        onClick={() => {
+          renderText();
+        }}
+      >
+        Button
+      </Button>
+      {click ? <p>Andreas er kul</p> : null}
     </ChakraProvider>
-  )
+  );
 }
 
-export default App
+export default App;
