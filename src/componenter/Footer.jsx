3092b0526beb7c736f852bc29c4e1f487109099f
Andreas Christiansen <andycchhrr@gmail.com>
2024-05-02 20:04:53 +0200

    gh icon

--- src/componenter/Footer.jsx
+++ src/componenter/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
   return (
     <footer>
       <p>Andreas Christiansen 2024</p>
+      <p>Contact me: <a href="mailto:andrchr@stud.ntnu.no">andrchr@ntnu.no</a></p>
     </footer>
   );
 }
--- src/componenter/Nav.jsx
+++ src/componenter/Nav.jsx
@@ -5,12 +5,19 @@ function Nav() {
     <header>
       <nav>
         <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
+          <div className="nav-inner-left">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+          </div>
+          <div className="nav-inner-right">
+            <a target="_blank" href="https://github.com/AndrChr0">
+            <img className="github-icon-nav" src="/icons/github.svg" alt="GitHub icon" />
+            </a>
+          </div>
         </ul>
       </nav>
     </header>
