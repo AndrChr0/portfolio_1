0aa89ac897970037c35e5239f5680fb462abd7c7
Andreas Christiansen <andycchhrr@gmail.com>
2025-01-22 11:26:11 +0100

    feat(Hero): add background video to Hero component style(App.css): uncomment mobile first styles and set body background color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,13 @@ import React from "react";
 function Hero() {
   return (
     <div className=''>
+      <video
+        src='/portfolio_hero_vid.mp4'
+        className='h-screen'
+        autoPlay
+        loop
+        muted
+      ></video>
       <div tabIndex={0}>
         <h1 className=''>Andreas Christiansen</h1>
         <p>Web Developer</p>
