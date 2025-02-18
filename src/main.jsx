7c017e2588ffb776d40bd7fdfa194b13223ea5e9
Andreas Christiansen <andycchhrr@gmail.com>
2025-02-18 08:17:57 +0100

    chore: remove dotenv import from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./Index.css";
 
 import { BrowserRouter } from "react-router-dom";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import { createClient } from "@sanity/client";
 
 import imageUrlBuilder from "@sanity/image-url";
